6a1fc758749bc1b086f7feedf7432e6821f6e292
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-06 21:39:25 +0000

    updated 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python3 
-import random 
+#!/usr/bin/python3
+import random
 number = random.randint(-10000, 10000)
-print(f'{number} ',end="")
+print(f'{number} ', end="")
 if number > 0:
-  print('is positive')
+    print('is positive')
 elif number == 0:
-  print('is zero')
+    print('is zero')
 else:
-  print('is negative')
-  
-
-  
-    
+    print('is negative')
