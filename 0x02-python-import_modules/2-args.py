31d21aa7f7e182fdd4c80910e9da71637f8a6f88
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-09 01:07:48 +0000

    Updated 4 .py files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,12 @@ if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 1:
-            print("0 arguments.")
+        print("0 arguments.")
     elif len(argv) == 2:
-            print("1 argument:")
-            for i in range(1, len(argv)):
-                print(f"{i}: {argv[i]}")
+        print("1 argument:")
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
     elif len(argv) > 2:
-            print(f"{len(argv) - 1} arguments:")
-            for i in range(1, len(argv)):
-                print(f"{i}: {argv[i]}")
- 
+        print(f"{len(argv) - 1} arguments:")
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
