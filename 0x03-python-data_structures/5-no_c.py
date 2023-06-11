9fc52fead86493cc8f0b14e666b9b35854ffb4fe
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-11 21:03:49 +0000

    Updated 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char == "c" or char == "C":
+            pass
+        else:
+            new_string += char
+    
+    return new_string
 
-    print("{}".format(a))
