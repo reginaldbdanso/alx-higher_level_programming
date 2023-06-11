6a05cdaaa8d16e0081289629f212af10364eea20
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-11 17:19:38 +0000

    Updated 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4.pyc
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list.copy()
+    if idx >= len(my_list):
+        return my_list.copy()
 
-    for name in names:
-        print(name)
+    new_list = my_list.copy()
+    new_list.pop(idx)
+    new_list.insert(idx, element)
+    return new_list
