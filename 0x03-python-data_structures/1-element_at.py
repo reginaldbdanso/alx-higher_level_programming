ef8c86134e348e4aa8b9424156b622604ce375d0
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-11 09:45:58 +0000

    Updated 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    
+    return my_list[idx]
