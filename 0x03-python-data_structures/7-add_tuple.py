6b1a230d50e7dd02758858157ff4c196a0a50048
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-12 23:23:17 +0000

    Added 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-def add(a, b):
-    return (a + b)
+def add_tuple(tuple_a=(), tuple_b=()):
+    list_a, list_b = list(tuple_a), list(tuple_b)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            list_a.append(0)
+        list_a.append(0)
 
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            list_b.append(0)
+        list_b.append(0)
 
-def sub(a, b):
-    return (a - b)
+    if len(tuple_a) > 2:
+        list_a = list_a[:2]
+    if len(tuple_b) > 2:
+        list_b = list_b[:2]
 
-
-def mul(a, b):
-    return (a * b)
-
-
-def div(a, b):
-    return int(a / b)
+    new_list = [list_a[0] + list_b[0], list_a[1] + list_b[1]]
+    new_tuple = tuple(new_list)
+    return new_tuple
