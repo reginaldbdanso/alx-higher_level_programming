7d7dbf9a04f91191e3cd25c12bd6aba8710e6cb0
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-14 13:54:26 +0000

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4.pyc
+!/usr/bin/python3
 
-    for name in names:
-        print(name)
+import hidden_4
+
+if __name__ == "__main__":
+    for name in dir(hidden_4):
+        if "__" not in name:
+            print(name)
