7005acb8914067c062618ebae91bc50f806e31fd
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-14 22:58:42 +0000

    Updated 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix == [[]]:
-        print()
-    else:
-        new_matrix = matrix.copy()
-        for i in range(len(new_matrix)):
-            for j in range(len(new_matrix[i])):
-                # if j == len(matrix[i]) - 1:
-                #     print('{:d}'.format(new_matrix[i][j]))
-                # else:
-                # print('{:d} '.format(new_matrix[i][j]), end='')
-                new_matrix[i][j] **= 2
-    return new_matrix
+    # for i in matrix:
+    #     for j in matrix[i]:
+    #         j = j**2
+    return [[j**2 for j in i] for i in matrix]
