8b5c03fc4a93ae8c5abf88a09ea9ec14f910b268
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-14 23:36:19 +0000

    Updated file 2 to 2-unique_add.py

--- 0x04-python-more_data_structures/3-print_reversed_list_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if type(my_list) is not list:
-        pass
-    else:
-        my_list.reverse()
-        for items in my_list:
-            print("{:d}".format(items))
