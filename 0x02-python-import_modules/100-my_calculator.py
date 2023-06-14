05ac0f6561b319f02e5a636be6aac1a5cdf7d61d
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-14 14:58:45 +0000

    Updated 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,13 @@
-if __name__ == __main__:
+if __name__ == "__main__":
 #!/usr/bin/python3
 import sys
 if sys.argc != 3:
 print("Usage: ./100-my_calculator.py <a> <operator> <b>")
 exit(1)
-the operator is not one of the above:
-print Unknown operator. Available operators: +, -, * and / followed with a new line
-exit with the value 1
-You can cast a and b into integers by using int() (you can assume that all arguments will be castable into integers)
-The result should be printed like this: <a> <operator> <b> = <result>, followed by a new line
+if operator is not +,-,*,/:
+print("Unknown operator. Available operators: +, -, * and /")
+exit(1)
+a, b, operator = int(sys.argv[1]), int(sys.argv[3], sys.argv[2]
+result = a operator b
+You can cast a and b i int() (you can assume that all arguments will be castable into integers)
+print(f" {a} {operator} {b} = {result}")
