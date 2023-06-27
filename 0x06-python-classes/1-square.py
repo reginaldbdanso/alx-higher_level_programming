ab1768657ea4e860c00c41eaa265e7b35ce4e571
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-27 23:37:25 +0000

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module defines the Square class."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialize the Square object.
+        Args:
+            size - size of the square
+        """
+        self.__size = size
