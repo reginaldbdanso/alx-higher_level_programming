990f887391f09f98ba35cae20c968270d29e11bc
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-27 23:37:37 +0000

    Added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines a square"""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize an instance of a square
+
+        Args:
+            size - size of the square
+            and defaults to 0 if nothing is passed.
+            Must be an integer not less than 0.
+
+        Raises:
+            TypeError - when size passed is not an integer.
+            ValueError - when size is less than 0."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
