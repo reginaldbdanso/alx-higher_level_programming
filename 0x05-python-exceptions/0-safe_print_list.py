1572bb951a7d13c9c09eea0707e7e61596235d3d
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-06-29 00:49:35 +0000

    Updated 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    counter = 0
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
-            count += 1
+            print(my_list[i], end="")
+            counter += 1
     except IndexError:
         pass
-    finally:
-        print()
-        return count
+    print()
+    return counter
