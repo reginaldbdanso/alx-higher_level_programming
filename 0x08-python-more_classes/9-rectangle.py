378dd54249c611e3c344d133d3092d1c408b35d8
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-03 23:42:34 +0000

    Updated 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,6 +15,17 @@ class Rectangle:
             height - height of the rectangle
             defaults to 0 if nothing is passed.
             Must be an integer not less than 0."""
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
         Rectangle.number_of_instances += 1
         self.__height = height
         self.__width = width
