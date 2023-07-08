1e0cc99ea9ddbfbe306dd1eb1c09bc6f63361a0a
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-08 00:26:28 +0000

    Updated 0,2,4,5 txt files and py file 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,11 @@
 
 
 def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    argument: text: The input text to be processed
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
