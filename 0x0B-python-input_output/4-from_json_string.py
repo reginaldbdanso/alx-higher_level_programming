8293e48545033e40fb8725a966159c1cb117ce4e
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-11 21:47:18 +0000

    Updated 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -13,6 +13,6 @@ def from_json_string(my_str):
     represented by a JSON string
 
     Args:
-        @my_str: object to be converted
+        @my_str: JSON string to be converted
     """
     return json.loads(my_str)
