1d8eb8362609c4c19c808f6c0983047992ec4bbc
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-11 21:29:17 +0000

    Updated 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 """
-    module validates if the instance of an object of a class
-    is inherited directly or indirectly from a specified class
+json module imported
+The from_json_string module
 """
+import json
 
 
-def inherits_from(obj, a_class):
+def from_json_string(my_str):
     """
-    inherits_from:
-        checks if object is the same instance of an
-        object of a class is inherited directly
-        or not
+    from_json_string funcion
+    returns an object (Python data structure)
+    represented by a JSON string
 
     Args:
-        obj: passed object to be checked
-        a_class: passed class to be compared
-
-    Return:
-        True if the type of object is a subclass of
-        a class. Otherwise, False.
+        @my_str: object to be converted
     """
-    if (issubclass(type(obj), a_class) and type(obj) != a_class):
-        return True
-    else:
-        return False
+    return json.loads(my_str)
