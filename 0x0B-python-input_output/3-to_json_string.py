40b7338743ba8baf7cb7033dc579fdacc82923a9
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-11 21:09:48 +0000

    Updated 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
+import json
 """
-module validates whether an object is an instance
-of, or is an instance of a class inherited from
-"""
+json module imported
+The to_json_string module"""
 
 
-def is_kind_of_class(obj, a_class):
-    """
-    is_kind_of_class:
-        checks if obj is the same insatance as class
-        or of the parent class
-
-    Args:
-        obj - passed object to be checked
-        a_class - passed class to be compared
-
-    Return:
-        True if the object is exactly as class
-        otherwise, False.
-    """
-    if isinstance(obj, a_class):
-        return True
-
-    else:
-        return False
+def to_json_string(my_obj):
+    """to_json_string funcion definition"""
+    json_string = json.dumps(my_obj)
+    return (json_string)
