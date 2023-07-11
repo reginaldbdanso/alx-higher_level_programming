06cd14efc55d46bfbf01c4fd3c5c2d8664387550
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-11 23:59:34 +0000

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-""" a BaseGeometry class"""
+"""
+json module imported
+The load_from_json_file module
+"""
+import json
 
 
-class BaseGeometry:
-    """defines a base geometry class"""
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
+def load_from_json_file(filename):
+    """
+    load_from_json_file funcion
+    returns an object (Python data structure)
+    represented by a JSON string
+
+    Args:
+        @filename: filename or path
+    """
+
+    with open(filename, "r") as file:
+        json_str = file.read()
+        obj = json.loads(json_str)
+        return obj
