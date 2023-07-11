55ee285d9cde5c6962a3d61bc3288deea2ae7156
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-11 21:24:19 +0000

    Updated 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,5 +7,11 @@ The to_json_string module
 
 
 def to_json_string(my_obj):
-    """to_json_string funcion definition"""
+    """
+    to_json_string funcion
+    returns the JSON representation of an object
+
+    Args:
+        @my_obj: object to be converted
+    """
     return json.dumps(my_obj)
