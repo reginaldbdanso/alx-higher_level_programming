5b219f126aae2ddf0d63c55223fd03d338a9b163
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-11 21:47:31 +0000

    Updated 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-""" an empty class"""
+"""
+json module imported
+The save_to_json_file module
+"""
+import json
 
 
-class BaseGeometry:
-    """does nothing"""
-    pass
+def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file funcion
+    returns an object (Python data structure)
+    represented by a JSON string
+
+    Args:
+        @my_obj: object to be converted
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
+    
