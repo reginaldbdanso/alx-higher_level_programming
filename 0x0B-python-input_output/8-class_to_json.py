417bb1c464ab1238b2b97962f2c21c8b342267fb
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-12 00:54:53 +0000

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""the rectangle module"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+json module imported
+The add_item module
+"""
 
 
-class Rectangle(BaseGeometry):
-    """defines rectangle class"""
-    def __init__(self, width, height):
-        """constructor for Rectangle instance"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+def class_to_json(obj):
+    """
+    add_item - adds all items and saves to a JSON file
+    Args:
+        @args: arguments that to be added.
+        @filename: file to be updated
+    """
 
-        self.__width = width
-        self.__height = height
+    if isinstance(obj, (str, int, bool)):
+        return obj
+    elif isinstance(obj, list):
+        return [class_to_json(item) for item in obj]
+    elif isinstance(obj, dict):
+        return {key: class_to_json(value) for key, value in obj.items()}
+    else:
+        attributes = {}
+        for attr in dir(obj):
+            if not attr.startswith("__") and not callable(getattr(obj, attr)):
+                attributes[attr] = class_to_json(getattr(obj, attr))
+        return attributes
