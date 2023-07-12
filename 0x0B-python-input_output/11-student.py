cb061e5f5581fc1c3b9e62ce64442ee96ae6818e
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-07-12 19:01:31 +0000

    Updated 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,23 +1,61 @@
 #!/usr/bin/python3
-"""the Square module"""
-Rectangle = __import__('9-rectangle').Rectangle
+"""
+a class Student that defines a student
+"""
 
 
-class Square(Rectangle):
-    """Defines a square class"""
-    def __init__(self, size):
-        """constructor for the size instance"""
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
+class Student:
+    """
+    This is the student class
+    """
 
-    def area(self):
-        """returns the area of the square"""
-        return self.__size * self.__size
-
-    def __str__(self):
+    def __init__(self, first_name, last_name, age):
         """
-        returns the string representation
-        of the square object
+        INitializes a student instance
+        Args:
+            first_name: The first name of the student.
+            last_name: The last name of the student.
+            age: The age of the student.
         """
-        return f"[{type(self).__name__}] {self.__size}/{self.__size}"
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs:
+                A list of attribute names to be retrieved.
+                If not provided or None, all attributes are
+                retrieved.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+                If attrs is provided, only the specified attributes
+                are included.
+        """
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+
+        filtered_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_dict[attr] = getattr(self, attr)
+        return filtered_dict
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+
+        Args:
+            json:
+                A dictionary representing the attributes of the
+                Student instance.
+                The dictionary keys are the attribute names, and
+                the values are the attribute values.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
