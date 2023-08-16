cbcecd2f89961910727ad4bb6425cd8410b5f09e
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-08-16 09:09:01 +0000

    Updated 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- Lists all records in the table second_table with a score >= 10
 -- in descending order.
 
-SELECT `score`, `name` FROM `second_table`
-WHERE `score` >= 10 ORDER BY `score` DESC;
+SELECT score, name FROM second_table
+WHERE score >= 10 ORDER BY score DESC;
