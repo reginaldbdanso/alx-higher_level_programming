ac3fd1261d2df39a8621692b3e6f0a6ba37c88ff
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-08-16 23:03:22 +0000

    Updated 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- creates the MySQL server user user_0d_1 and grant all priviledges
-
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- A script that creates the MySQL server user user_0d_1
+-- and assign password and granted all privileges to it
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' WITH GRANT OPTION;
