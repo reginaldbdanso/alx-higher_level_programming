f34e475cc434074e371aabcff76c9958fd7509de
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-11 23:01:51 +0000

    Updated 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(parseInt(process.argv[2]))) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
 for (let i = 0; i < process.argv[2]; i++) {
   console.log('C is fun');
