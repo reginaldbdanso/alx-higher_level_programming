e00b60095c1c7cfea58083a816408f9ede17395e
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-11 22:04:31 +0000

    Updated 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
+// import { process.argv } from 'node:process';
 
-if (argv.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
