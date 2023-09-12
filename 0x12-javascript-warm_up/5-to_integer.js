20b04cf7cd6432ed93491c50636e7c16196d96f8
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-12 06:03:04 +0000

    Updated 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (isNaN(parseInt(process.argv[2])) || process.argv[2] === null) {
+if (isNaN(process.argv[2]) || process.argv[2] === null) {
   console.log('Not a number');
 } else if (process.argv[2] < 0) {
   // pass
