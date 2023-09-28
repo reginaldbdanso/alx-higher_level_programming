e81df4693be4336d5fd6d6e341318ad6e9517401
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-28 23:09:49 +0000

    Updated 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Send a DELETE request to a given URL and display the response body.
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
