f6d81c6f06308011c7ca396f3e378b505fcd447c
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-09-29 11:52:29 +0000

    Updated 0-hbtn_status and 1-hbtn_header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,13 @@
 import urllib.request
 
 if __name__ == "__main__":
+    # Get the url from the command line
     url = 'https://alx-intranet.hbtn.io/status'
+    # Send a request to the URL using urllib
     with urllib.request.urlopen(url) as response:
+        # Get the body of the response
         html = response.read()
+        # Print the body of the response
         print("Body response:")
         print(f"\t- type: {type(html)}")
         print(f"\t- content: {html}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,7 +8,11 @@ import sys
 
 
 if __name__ == "__main__":
+    # Get the url from the command line
     url = sys.argv[1]
+    # Send a request to the URL
     with urllib.request.urlopen(url) as response:
+        # Get the header of the response
         header = response.info()
+        # Print the value of the X-Request-Id variable found in the header
         print(header.get('X-Request-Id'))
