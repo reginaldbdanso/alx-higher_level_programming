8e6c0b0968f68ac95d704ff65c981f2afe46f7c8
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2024-02-17 00:32:23 +0000

    Updated 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     """Adds 2 integers"""
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a is None:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float)) or b is None:
         raise TypeError("b must be an integer")
 
     if isinstance(a, float):
